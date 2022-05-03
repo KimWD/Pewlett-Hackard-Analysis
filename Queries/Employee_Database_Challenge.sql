d498fe8dd95545c94e27aebcd6032d82b9bbd7df
Kimberly Wagner-Dabbour <kimberlydabbour@gmail.com>
2022-05-03 12:56:02 -0400

    update

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,5 +1,4 @@
---Follow the instructions below to complete Deliverable 1.
-
+--Deliverable 1: The Number of Retiring Employees by Title 
 --Retrieve the emp_no, first_name, and last_name columns from the Employees table.
 --Retrieve the title, from_date, and to_date columns from the Titles table.
 --Create a new table using the INTO clause.
@@ -24,6 +23,7 @@ order by e.emp_no;
 --Exclude those employees that have already left the company by filtering on to_date to keep only those dates that are equal to '9999-01-01'.
 --Create a Unique Titles table using the INTO clause.
 --Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e., to_date) of the most recent title.
+--Export the Unique Titles table as unique_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
 
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -38,9 +38,41 @@ ORDER BY emp_no, to_date DESC;
 --First, retrieve the number of titles from the Unique Titles table.
 --Then, create a Retiring Titles table to hold the required information.
 --Group the table by title, then sort the count column in descending order.
+--Export the Retiring Titles table as retiring_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
 
 SELECT COUNT(ut.title), ut.title
 INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+
+-- In the Employee_Database_challenge.sql file, write a query to create a Mentorship Eligibility table that holds the employees who are eligible to participate in a mentorship program.
+-- Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+-- Retrieve the from_date and to_date columns from the Department Employee table.
+-- Retrieve the title column from the Titles table.
+-- Use a DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+-- Create a new table using the INTO clause.
+-- Join the Employees and the Department Employee tables on the primary key.
+-- Join the Employees and the Titles tables on the primary key.
+-- Filter the data on the to_date column to all the current employees, then filter the data on the birth_date columns to get all the employees whose birth dates are between January 1, 1965 and December 31, 1965.
+-- Order the table by the employee number.
+
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- Export the Mentorship Eligibility table as mentorship_eligibilty.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.

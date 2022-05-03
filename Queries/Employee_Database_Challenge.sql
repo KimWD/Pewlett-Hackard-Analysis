d0bb7eeeee4798789bc9d7406b37ce5231450361
Kimberly Wagner-Dabbour <kimberlydabbour@gmail.com>
2022-05-03 12:20:26 -0400

    updates

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,46 @@
+--Follow the instructions below to complete Deliverable 1.
+
+--Retrieve the emp_no, first_name, and last_name columns from the Employees table.
+--Retrieve the title, from_date, and to_date columns from the Titles table.
+--Create a new table using the INTO clause.
+--Join both tables on the primary key.
+--Filter the data on the birth_date column to retrieve the employees who were born between 1952 and 1955. Then, order by the employee number.
+
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+--Export the Retirement Titles table from the previous step as retirement_titles.csv and save it to your Data folder in the Pewlett-Hackard-Analysis folder.
+
+-- Use the DISTINCT ON statement to retrieve the first occurrence of the employee number for each set of rows defined by the ON () clause.
+--Exclude those employees that have already left the company by filtering on to_date to keep only those dates that are equal to '9999-01-01'.
+--Create a Unique Titles table using the INTO clause.
+--Sort the Unique Titles table in ascending order by the employee number and descending order by the last date (i.e., to_date) of the most recent title.
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = ('9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- Write another query in the Employee_Database_challenge.sql file to retrieve the number of employees by their most recent job title who are about to retire.
+--First, retrieve the number of titles from the Unique Titles table.
+--Then, create a Retiring Titles table to hold the required information.
+--Group the table by title, then sort the count column in descending order.
+
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY count DESC;
---
+++ Queries/queries_challenge.sql
@@ -0,0 +1,13 @@
+-- Deliverable 1 Requirements
+-- You will earn a perfect score for Deliverable 1 by completing all requirements below:
+
+-- A query is written and executed to create a Retirement Titles table for employees who are born between January 1, 1952 and December 31, 1955. (10 pt)
+SELECT * 
+FROM retirement_titles
+in between
+
+-- The Retirement Titles table is exported as retirement_titles.csv. (5 pt)
+-- ​A query is written and executed to create a Unique Titles table that contains the employee number, first and last name, and most recent title. (15 pt)
+-- ​The Unique Titles table is exported as unique_titles.csv. (5 pt)
+-- A query is written and executed to create a Retiring Titles table that contains the number of titles filled by employees who are retiring. (10 pt)
+-- The Retiring Titles table is exported as retiring_titles.csv. (5 pt)
